be4ca2fec4247043353a91fd3b2d25869ee06b88
Creatoria <1612706077@qq.com>
2021-02-20 22:27:26 +0800

    complement on hiding in phase

--- util.py
+++ util.py
@@ -6,9 +6,14 @@ from Crypto.PublicKey import RSA
 from Crypto.Signature import pkcs1_15
 from Crypto.Hash import SHA256
 from Crypto.Util.Padding import pad, unpad
+from Crypto.Util.strxor import strxor
 import random
 import struct
 from reedsolo import RSCodec
+import warnings
+import os
+
+warnings.filterwarnings('ignore')
 
 _check = 32
 _rsc_block = 255 - _check
@@ -71,20 +76,39 @@ class Cryptor():
             if key is None:
                 raise Exception('no key provided')
             self.key = RSA.import_key(open(key).read())
-        if self.debug:
-            print(self.block_size)
 
     def decrypt(self, data: bytes, hashalg=SHA256):
-        bb = self._unhead(data[:_header_size])
+        broken = False
+        try:
+            bb = self._unhead(data[:_header_size])
+        except:
+            print(data)
+            return b'\x00', True
         _rrs = RSCodec(_check)
         dd = data[_header_size:bb * 255 + _header_size]
-        print(_rrs.check(dd))
-        dd = unpad(_rrs.decode(dd)[0], _rsc_block)
+        if self.debug:
+            print(_rrs.check(dd))
+        kd = b''
+        for i in range(bb):
+            try:
+                kd += _rrs.decode(dd[i * 255:(i + 1) * 255])[0]
+            except:
+                kd += dd[i * 255:(i + 1) * 255 - _check]
+                broken = True
+        try:
+            dd = unpad(kd, _rsc_block)
+        except:
+            dd = kd[:-kd[-1]]
+            broken = True
         if self.mode == MODE_AES:
             dd = self._aes_dec(dd)
         else:
             dd = dd
-        dd = unpad(dd, self.block_size)
+        try:
+            dd = unpad(dd, self.block_size)
+        except:
+            dd = dd[:-dd[-1]]
+            broken = True
         if self.verify:
             sz = self.key.size_in_bytes()
             dd, sig = dd[:-sz], dd[-sz:]
@@ -93,7 +117,9 @@ class Cryptor():
                 pkcs1_15.new(self.key).verify(hs, sig)
             except:
                 print('signature not valid')
-        return dd
+        if broken:
+            print('data may be broken during the process')
+        return dd, broken
 
     def encrypt(self, data: bytes, hashalg=SHA256):
         if self.verify:
@@ -120,7 +146,10 @@ class Cryptor():
 
     def _unhead(self, hh: bytes):
         _rrs = RSCodec()
-        hd = _rrs.decode(hh)[0]
+        try:
+            hd = _rrs.decode(hh)[0]
+        except:
+            hd = hh[:-10]
         if hd[:5] != b'FsTeg':
             raise Exception('Unknown header type')
         ll = 1 + int.from_bytes(hd[8:12], 'little') * \
@@ -143,18 +172,23 @@ def extract(audiofile: str, ths=2048, perseg=441, overlap=0):
                       'rect', perseg, overlap)
     i = 0
     d = ''
+    of = open('ex.log', 'w')
     while i < bb:
         cl = zxxl[:, i]
         cr = zxxr[:, i]
         idxl = np.argwhere(np.abs(cl) > ths)
         idxr = np.argwhere(np.abs(cr) > ths)
+        # if len(idxl.tolist()) + len(idxr.tolist()) == 0:
+        #     i += 1
+        #     continue
         for j in idxl:
-            d += _gp_by_amp(cl[j])
+            d += _gp_by_ang(cl[j])
         for j in idxr:
-            d += _gp_by_amp(cr[j])
-        # print('left channel:', [list(f[idxl].T[0]), list(np.abs(cl[idxl].T[0]).astype(int))],
-        #       ',right channel:', [list(f[idxr].T[0]), list(np.abs(cr[idxr].T[0]).astype(int))], '@', t[i])
+            d += _gp_by_ang(cr[j])
+        of.write('L: {} {}, R: {} {} @{}\n'.format(list(f[idxl].T[0]), list(np.angle(
+            cl[idxl].T[0], 1).astype(int)), list(f[idxr].T[0]), list(np.angle(cr[idxr].T[0], 1).astype(int)), str(t[i])[:str(t[i]).index('.') + 3]))
         i += 1
+    of.close()
     return _bin2byt(d)
 
 
@@ -171,23 +205,27 @@ def embeed(mess: EMess, infile: str, outfile: str, ths=2048, perseg=441, overlap
     ll = 0
     i = 0
     lm = len(mess) * 8
+    of = open('em.log', 'w')
     while ll < lm:
         cpl = zxxl[:, i].copy()
         cpr = zxxr[:, i].copy()
         idxl = np.argwhere(np.abs(cpl) > ths)
         idxr = np.argwhere(np.abs(cpr) > ths)
-        # print('left channel:', [list(f[idxl].T[0]), list(np.abs(cpl[idxl].T[0]).astype(int))],
-        #       ',right channel:', [list(f[idxr].T[0]), list(np.abs(cpr[idxr].T[0]).astype(int))], '@', t[i])
+        # if len(idxl.tolist()) + len(idxr.tolist()) == 0:
+        #     i += 1
+        #     continue
+        of.write('L: {} {}, R: {} {} @{}\n'.format(list(f[idxl].T[0]), list(np.angle(
+            cpl[idxl].T[0], 1).astype(int)), list(f[idxr].T[0]), list(np.angle(cpr[idxr].T[0], 1).astype(int)), str(t[i])[:str(t[i]).index('.') + 3]))
         for j in range(min(len(idxl), lm - ll)):
-            cpl[idxl[j]] = _hb_by_amp(cpl[idxl[j]], next(mess))
+            cpl[idxl[j]] = _hb_by_ang(cpl[idxl[j]], next(mess))
             ll += 1
         for j in range(min(len(idxr), lm - ll)):
-            cpr[idxr[j]] = _hb_by_amp(cpr[idxr[j]], next(mess))
+            cpr[idxr[j]] = _hb_by_ang(cpr[idxr[j]], next(mess))
             ll += 1
         sl.append(cpl)
         sr.append(cpr)
-
         i += 1
+    of.close()
     print('blocks used:', i, 'total:', bb)
     _, rsl = istft(np.array(sl).T, srt, 'rect', perseg, overlap)
     _, rsr = istft(np.array(sr).T, srt, 'rect', perseg, overlap)
@@ -197,7 +235,7 @@ def embeed(mess: EMess, infile: str, outfile: str, ths=2048, perseg=441, overlap
     wavfile.write(outfile, srt, fsg)
 
 
-def _hb_by_amp(nn: complex, b: str, delt=15):
+def _hb_by_amp(nn: complex, b: str, delt=256):
     i = nn.real
     j = nn.imag
     mm = np.abs(nn)
@@ -207,22 +245,30 @@ def _hb_by_amp(nn: complex, b: str, delt=15):
     return np.complex(i, j)
 
 
-def _gp_by_amp(tt: complex, delt=15):
+def _gp_by_amp(tt: complex, delt=256):
     bb = '1' if int(np.abs(tt) // delt) % 2 else '0'
     return bb
 
 
-def _hb_by_ang(nn: complex, b: str):
+def _hb_by_ang(nn: complex, b: str, delt=4):
     ll = np.abs(nn)
     aa = np.angle(nn, True)
-    if (int(aa) // 3 % 2) ^ int(b):
-        aa -= 3
-        return np.complex(np.sin(aa) * ll, np.cos(aa) * ll)
+    gg = int(aa) // delt
+    if (gg % 2) ^ int(b):
+        s1 = (2 * gg - 1) * delt / 2
+        s2 = (2 * gg + 3) * delt / 2
+        if aa - s1 > s2 - aa:
+            aa = s2
+        else:
+            aa = s1
+    else:
+        aa = (2 * gg + 1) * delt / 2
+    return np.complex(np.cos(aa * np.pi / 180) * ll, np.sin(aa * np.pi / 180) * ll)
     return nn
 
 
-def _gp_by_ang(nn: complex):
-    return '1' if (int(np.angle(nn, True)) // 3) % 2 else '0'
+def _gp_by_ang(nn: complex, delt=4):
+    return '1' if (int(np.angle(nn, True)) // delt) % 2 else '0'
 
 
 def _bin2byt(s: str):
@@ -251,26 +297,51 @@ def estimate(audiofile: str, ths=2048, perseg=441, overlap=0):
     return u // 8
 
 
+def convert():
+    if not os.popen('ffmpeg'):
+        pass
+
+
+def mis(out, raw):
+    st = len(raw) - len(out)
+    rt = raw[:len(out)]
+    mi = EMess(strxor(out, rt))
+    cc = 0
+    i = 0
+    while i < len(mi) * 8:
+        cc += int(next(mi))
+        i += 1
+    return st / len(raw), cc / len(out) / 8
+
+
 if __name__ == '__main__':
     infile = 'test/wavs/01.wav'
     outfile = 'out.wav'
     privk = 'test/test.key'
+    pubk = 'test/test.pub.key'
     # print(estimate(infile), 'bytes available.')
 
-    # c = Cryptor(MODE_AES, debug=0, password=b'123456')
+    c = Cryptor()
+    d = Cryptor(debug=True)
+
+    # c = Cryptor(MODE_AES, password=b'123456')
     # d = Cryptor(MODE_AES, password=b'123456')
 
-    c = Cryptor(MODE_AES, password=b'123456',
-                verify=True, key='test/test.key')
-    d = Cryptor(MODE_AES, password=b'123456',
-                verify=True, key='test/test.pub.key')
+    # c = Cryptor(MODE_AES, password=b'123456',
+    #             verify=True, key=privk)
+    # d = Cryptor(MODE_AES, password=b'123456',
+    #             verify=True, key=pubk)
 
-    mm = random.randbytes(4096)
-    # mm = b'hello' * 100
+    # mm = random.randbytes(1024)
+    mm = b'hello' * 200
     ee = c.encrypt(mm)
     # print(ee, len(ee))
     embeed(EMess(ee), infile, outfile, ths=1600)
-    dd = extract(outfile, ths=1600)
+    # os.system('ffmpeg -i out.wav -b:a 320k o1.mp3 -y')
+    # os.system('ffmpeg -i o1.mp3 o2.wav -y')
+    dd = extract('out.wav', ths=1600)
     # print(dd[:len(ee)], len(dd))
-    dd = d.decrypt(dd)
-    print(dd == mm)
+    dd, fg = d.decrypt(dd)
+    los, ber = mis(dd, mm)
+    print(dd)
+    print(dd == mm, 'lost={},ber={}'.format(los, ber))
